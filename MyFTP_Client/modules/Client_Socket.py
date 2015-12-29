8d7e0ebcfa5f6939253a99bf99cf7cd4f80a7ed3
ALan <alanwmail@163.com>
2015-12-29 14:19:56 +0800

    201512291419

--- MyFTP_Client/modules/Client_Socket.py
+++
@@ -1,8 +0,0 @@
-#-*- coding:utf-8 -*-
-#/usr/bin/env python
-import socket
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host = ('127.0.0.1',8080)
-client_socket.connect(host)
-welcome_info = client_socket.recv(1024)
-print welcome_info
